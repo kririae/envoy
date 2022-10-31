7b02d5b59f0a039728bde7043fa24067e3f50f85
kririae <kriaeth@outlook.com>
2022-10-31 22:15:07 +0800

    add README.md

--- assets/fetch_assets.sh
+++ assets/fetch_assets.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 url="https://file.yuyuko.cc/envoy"
 assets=("bun_zipper_res4.ply" "sphere.ply" "watertight.ply")
-if test -z "$1"
-then
-  base_dir="."
-else
-  base_dir="$1"
-fi
 for asset in "${assets[@]}"; do
-  echo "saving file to $base_dir/$asset"
-  curl "$url/$asset" > "$base_dir/$asset"
+  curl "$url/$asset" > "$asset"
 done
